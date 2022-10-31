814de744e9c0e8c606bd8c11152f889a9715ab37
Ceceskii <97866655+Ceceskii@users.noreply.github.com>
2022-10-31 14:01:22 -0700

    Add debug

--- debug.py
+++ debug.py
@@ -9,7 +9,6 @@ def lone_sum(a, b, c):
         return 0
     else:
         return a+b+c
-
 print("lone_sum of 10, 10, 10 should be 0: " + str(lone_sum(10, 10, 10)))
 print("lone_sum of 1, 2, 3 should be 6: " + str(lone_sum(1, 2, 3)))
 print("lone_sum of 1, 2, 1 should be 2: " + str(lone_sum(1, 2, 1)))
